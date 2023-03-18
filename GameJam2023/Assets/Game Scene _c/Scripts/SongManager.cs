9b8ff7d0ccd4a17b08b020fdecd0c55c6ef442c2
pluie-nord <pluie.nord@gmail.com>
2023-03-18 10:27:09 +0300

    Init pull

---
+++ GameJam2023/Assets/Game Scene _c/Scripts/SongManager.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    [System.Serializable]
+    public struct note
+    {
+        public float bit;
+        public string key;
+    }
+    private enum NoteStats
+    {
+        PERFECT,
+        NORAML,
+        MISTAKE
+    }
+    public AudioSource songToPlay;
+    public int beatsShownInAdvance;
+    float currentPos; //в секундах
+    float currentPos_beats; //в ударах
+    float secPerBeat; 
+    float dpsTimePlayed; //прошло времени с начала композиции
+    public float bpm; //ударов в минуту
+    public note[] notes;
+    public int nextNoteIndex = 0;
+
+    private void Start()
+    {
+        secPerBeat = 60f / bpm;
+        dpsTimePlayed = (float)AudioSettings.dspTime;
+        songToPlay.Play();
+    }
+
+    private void Update()
+    {
+        currentPos = (float)AudioSettings.dspTime - dpsTimePlayed;
+        currentPos_beats = currentPos/ secPerBeat;
+        if (nextNoteIndex < notes.Length && notes[nextNoteIndex].bit < currentPos_beats + beatsShownInAdvance)
+        {
+            //SpawnNote();
+            //(KeyCode)Enum.Parse(typeof(KeyCode), notes[nextNoteIndex].key))
+            if (Input.GetKey(KeyCode.W))
+            {
+                print("да");
+            }
+            nextNoteIndex++;
+        }
+    }
+
+    /*public NoteStats CheckNote()
+    {
+        if()
+    }*/
+
+}
