302e6a6b4fab4d9feecc8e62b7f38de68587fe88
pluie-nord <pluie.nord@gmail.com>
2023-03-19 01:10:34 +0300

    Set menu scene

---
+++ GameJam2023/Assets/Game Scene _c/Scripts/UI Scripts/LevelUIHendler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelUIHendler : MonoBehaviour
+{
+    public void ReturnToMenu()
+    {
+
+    }
+    public void RestartLevel()
+    {
+
+    }
+}
---
+++ GameJam2023/Assets/Game Scene _c/Scripts/UI Scripts/MenuUIHendler.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUIHendler : MonoBehaviour
+{
+    public void Exit()
+    {
+        
+    }
+    public void ChooseSong()
+    {
+
+    }
+}
