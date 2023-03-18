fc53b5ecd9cfd6cedff18765225154ed96526482
pluie-nord <pluie.nord@gmail.com>
2023-03-19 02:44:28 +0300

    Update

--- GameJam2023/Assets/Game Scene _c/Scripts/UI Scripts/MenuUIHendler.cs
+++ GameJam2023/Assets/Game Scene _c/Scripts/UI Scripts/MenuUIHendler.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class MenuUIHendler : MonoBehaviour
 {
+    [SerializeField] GameObject SongAarray;
+    [SerializeField] GameObject Menu;
     public void Exit()
     {
-        
     }
     public void ChooseSong()
     {
-
+        SongAarray.SetActive(true);
+        Menu.SetActive(false);
     }
 
     public void ShowNextSong()
@@ -30,7 +34,8 @@ public class MenuUIHendler : MonoBehaviour
 
     public void BackToMenu()
     {
-
+        SongAarray.SetActive(false);
+        Menu.SetActive(true);
     }
 
 }
