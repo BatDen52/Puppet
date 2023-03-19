e05727e3d921dccb1932d19f30ba346f1bc776a7
BatDen52 <kaluzhin.denis@gmail.com>
2023-03-19 04:59:59 +0300

    sound ligic fix

--- GameJam2023/Assets/Game Scene _c/Scripts/Audio/AudioEventManager.cs
+++ GameJam2023/Assets/Game Scene _c/Scripts/Audio/AudioEventManager.cs
@@ -10,6 +10,7 @@ public class AudioEventManager : MonoBehaviour
     [SerializeField] private AudioClip[] _soundsFail;
     [SerializeField] private AudioClip[] _soundsBroken;
     [SerializeField] private AudioClip[] _soundsSuccess;
+    [SerializeField] private AudioClip[] _soundsMove;
 
     private Score _score;
 
@@ -19,32 +20,45 @@ public class AudioEventManager : MonoBehaviour
         _score.Fail += OnFail;
         _score.Broken += OnBroken;
         _score.Success += OnSuccess;
+        _score.Success += OnMove;
+    }
+
+    public void PlayInFail(AudioClip clip)
+    {
+        _sourceFail.PlayOneShot(clip);
+    }
+
+    public void PlayInBroken(AudioClip clip)
+    {
+        _sourceBroken.PlayOneShot(clip);
+    }
+
+    public void PlayInSuccess(AudioClip clip)
+    {
+        _sourceSuccess.PlayOneShot(clip);
     }
 
     private void OnFail(int failScore)
     {
         if (_soundsFail.Length > 0)
-        {
-            _sourceFail.clip = _soundsFail[Random.Range(0, _soundsFail.Length)];
-            _sourceFail.Play();
-        }
+            PlayInFail(_soundsFail[Random.Range(0, _soundsFail.Length)]);
     }
 
-    private void OnBroken(int failScore)
+    private void OnBroken(int brokenScore)
     {
         if (_soundsBroken.Length > 0)
-        {
-            _sourceBroken.clip = _soundsBroken[Random.Range(0, _soundsBroken.Length)];
-            _sourceBroken.Play();
-        }
+            PlayInBroken(_soundsBroken[Random.Range(0, _soundsBroken.Length)]);
     }
 
-    private void OnSuccess(int failScore)
+    private void OnSuccess(int successScore)
     {
         if (_soundsSuccess.Length > 0)
-        {
-            _sourceSuccess.clip = _soundsSuccess[Random.Range(0, _soundsSuccess.Length)];
-            _sourceSuccess.Play();
-        }
+            PlayInSuccess(_soundsSuccess[Random.Range(0, _soundsSuccess.Length)]);
+    }
+
+    private void OnMove(int score)
+    {
+        if (_soundsMove.Length > 0)
+            PlayInSuccess(_soundsMove[Random.Range(0, _soundsMove.Length)]);
     }
 }
