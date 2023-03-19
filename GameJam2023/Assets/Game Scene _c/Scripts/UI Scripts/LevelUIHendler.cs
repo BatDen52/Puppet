2cfbba67aaf6077f5fc50e6d5a22ee9eeba29f33
BatDen52 <kaluzhin.denis@gmail.com>
2023-03-19 08:55:39 +0300

    end game panel

--- GameJam2023/Assets/Game Scene _c/Scripts/UI Scripts/LevelUIHendler.cs
+++ GameJam2023/Assets/Game Scene _c/Scripts/UI Scripts/LevelUIHendler.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelUIHendler : MonoBehaviour
 {
     public void ReturnToMenu()
     {
-
+        LoadScene(0);
     }
+
     public void RestartLevel()
     {
+        LoadScene(1);
+    }
 
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
